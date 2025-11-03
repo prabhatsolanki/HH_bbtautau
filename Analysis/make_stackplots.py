94fe8822cac27530e0eafea18b5ce94e430a10eb
Prabhat Solanki <prabhat.solanki@cern.ch>
2025-11-03 15:34:35 +0100

    Plotting for now

--- Analysis/make_stackplots.py
+++ Analysis/make_stackplots.py
@@ -1,27 +1,18 @@
 import os
 
-era = "ERA_string"
-ver = "your_version"
-indir = f"/eos/user/u/username/HH_bbtautau_Run3/histograms/{ver}/{era}/merged/"
-plotdir = f"/eos/user/u/username/HH_bbtautau_Run3/histograms/{ver}/{era}/plots/"
+era = "Run3_2022"
+ver = "v2510_2"
+indir = f"/eos/user/p/prsolank/HH_bbtautau_resonant_Run3/merged_hists/{ver}/{era}/"
+plotdir = f"/eos/user/p/prsolank/HH_bbtautau_resonant_Run3/merged_hists/{ver}/{era}/plots/"
 
-varnames = [
-    "tau1_pt",
-    "tau2_pt",
-    "b1_pt",
-    "b2_pt",
-    "tautau_m_vis",
-    "bb_m_vis",
-    "MT2",
-]  # "bbtautau_mass"
+#varnames = ["tau1_pt", "tau2_pt", "tau1_eta", "tau2_eta", "tautau_m_vis"] #"bbtautau_mass"
+varnames = ["tautau_m_vis"] #"bbtautau_mass"
 
-channellist = ["eE", "eMu", "muMu", "eTau", "muTau", "tauTau"]
+channellist = ["tauTau"]
 
 cat = "inclusive"
 
-using_uncertainties = (
-    True  # When we turn on Up/Down, the file storage changes due to renameHists.py
-)
+using_uncertainties = False #When we turn on Up/Down, the file storage changes due to renameHists.py
 
 for var in varnames:
     for channel in channellist:
@@ -31,14 +22,8 @@ for var in varnames:
         outname = os.path.join(plotdir, f"HHbbtautau_{channel}_{var}_StackPlot.pdf")
 
         if not using_uncertainties:
-            os.system(
-                f"python3 ../FLAF/Analysis/HistPlotter.py --inFile {filename} --bckgConfig ../config/background_samples.yaml --globalConfig ../config/global.yaml --outFile {outname} --var {var} --category {cat} --channel {channel} --uncSource Central --wantData --year {era} --wantQCD False --rebin False --analysis HH_bbtautau --qcdregion OS_Iso --sigConfig ../config/{era}/samples.yaml --wantSignals"
-            )
+            os.system(f"python3 ../FLAF/Analysis/HistPlotter.py --inFile {filename} --bckgConfig ../config/background_samples.yaml --globalConfig ../config/global.yaml --outFile {outname} --var {var} --category {cat} --channel {channel} --uncSource Central --wantData --year {era} --wantQCD True --rebin False --analysis HH_bbtautau --qcdregion OS_Iso --sigConfig ../config/{era}/samples.yaml --wantSignals")
 
         else:
-            filename = os.path.join(
-                indir, var, "tmp", f"all_histograms_{var}_hadded.root"
-            )
-            os.system(
-                f"python3 ../FLAF/Analysis/HistPlotter.py --inFile {filename} --bckgConfig ../config/background_samples.yaml --globalConfig ../config/global.yaml --outFile {outname} --var {var} --category {cat} --channel {channel} --uncSource Central --wantData --year {era} --wantQCD False --rebin False --analysis HH_bbtautau --qcdregion OS_Iso --sigConfig ../config/{era}/samples.yaml --wantSignals"
-            )
+            filename = os.path.join(indir, var, 'tmp', f"all_histograms_{var}_hadded.root")
+            os.system(f"python3 ../FLAF/Analysis/HistPlotter.py --inFile {filename} --bckgConfig ../config/background_samples.yaml --globalConfig ../config/global.yaml --outFile {outname} --var {var} --category {cat} --channel {channel} --uncSource Central --wantData --year {era} --wantQCD True --rebin False --analysis HH_bbtautau --qcdregion SS_AntiIso")
